68d395afcd93b5b9abe6e9aa16e98b2ec207ea10
cclzhang <hello@cecilezhang.com>
2020-02-13 15:08:19 -0500

    added basic structure

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 const lifeApp = {};
 
-$(function () {
+const $body = $("body");
+const $mainPage = $(".mainPage");
+const $menuButton = $(".menuButton");
+const $sleepButton = $(".sleep");
+const $runButton = $(".run");
+const $goalButton = $(".goals");
+const $cubingButton = $(".cubing");
 
+lifeApp.sleepOneHtml = `
+    <section class="sleepOne">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <button class="addSleep">Add Sleep</button>
+    </section>
+`;
+
+$sleepButton.on('click', function () {
+
+});
+
+
+
+
+
+
+
+
+
+lifeApp.init = () => {
+
+}
+
+$(function () {
+    lifeApp.init();
 });
