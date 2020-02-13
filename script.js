b3422c0276fd2c2eb989507e3d036872f62fc939
cclzhang <hello@cecilezhang.com>
2020-02-13 18:18:40 -0500

    added basic sleep pages

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
 const lifeApp = {};
 
 const $body = $("body");
-const $mainPage = $(".mainPage");
+const $wrapper = $(".wrapper");
 const $menuButton = $(".menuButton");
 const $sleepButton = $(".sleep");
 const $runButton = $(".run");
 const $goalButton = $(".goals");
 const $cubingButton = $(".cubing");
+const $addSleep = $(".addSleep");
+const $bedTime = $("input[name=bedTime]");
+const $wakeTime = $("input[name=wakeTime]");
+
+
+lifeApp.averageSleep = 7;
 
 lifeApp.sleepOneHtml = `
     <section class="sleepOne">
+        <h2>week to date</h2>
+        <h3>${lifeApp.averageSleep}hrs</h3>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
         </ul>
         <button class="addSleep">Add Sleep</button>
     </section>
 `;
 
-$sleepButton.on('click', function () {
+lifeApp.sleepTwoHtml = `
+    <section class="sleepTwo">
+        <form>
+            <label for="bedTime">What time did you sleep?</label>
+            <input id="bedTime" type="time" name="bedTime" required>
+            <label for="wakeTime">What time did you sleep?</label>
+            <input id="wakeTime" type="time" name="wakeTime" required>
+            <button type="submit" class="insertSleep">calculate</button>
+        </form>
+    </section>
+`;
 
+$sleepButton.on('click', function(){
+    $wrapper.html(lifeApp.sleepOneHtml);
+});
+
+$wrapper.on('click', '.addSleep', function(){
+    $wrapper.html(lifeApp.sleepTwoHtml);
+});
+
+$wrapper.on('click', '.insertSleep', function () {
+    $wrapper.html(lifeApp.sleepOneHtml);
+    console.log($bedTime.val(), $wakeTime.val());
 });
 
 
