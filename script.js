6e8141c7ed2fd476bfb8713780ee9664b5259d9b
cclzhang <hello@cecilezhang.com>
2020-02-13 23:26:55 -0500

    adding a caching method for sleep and basic outline for goal

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const $wrapper = $(".wrapper");
 const $menuButton = $(".menuButton");
 const $sleepButton = $(".sleep");
 const $runButton = $(".run");
-const $goalButton = $(".goals");
+const $goalsButton = $(".goals");
 const $cubingButton = $(".cubing");
 const $addSleep = $(".addSleep");
 const $bedTime = $("#bedTime");
@@ -15,13 +15,16 @@ const $sleepForm = $(".sleepTwo form");
 
 
 
-lifeApp.sleepTracker = [8, 6, 15, 0, 0, 0, 0];
+// -------------------- SLEEP -----------------------//
+const dayOfWeek = new Date().getDay();
 
 lifeApp.averageSleep = 7;
 
+// sleepArray[index] -> localStorage.getItem("day" + index);
+
 lifeApp.sleepOneHtml = `
     <section class="sleepOne">
-        <h2>week to date</h2>
+        <h2>sleep breakdown</h2>
         <h3>${lifeApp.averageSleep}hrs</h3>
         <ul>
             <li></li>
@@ -48,27 +51,29 @@ lifeApp.sleepTwoHtml = `
     </section>
 `;
 
-$sleepButton.on('click', function(){
+$sleepButton.on("click", function(){
     $wrapper.html(lifeApp.sleepOneHtml);
+    //console.log(localStorage.getItem("kys"));
+    lifeApp.updateGraph();  
 });
 
-$wrapper.on('click', '.addSleep', function(){
+$wrapper.on("click", ".addSleep", function(){
     $wrapper.html(lifeApp.sleepTwoHtml);
 });
 
-$wrapper.on('submit', $sleepForm, function(e){
+$wrapper.on("submit", $sleepForm, function(e){
     e.preventDefault();
     const $bedTime = $("#bedTime").val();
     const $wakeTime = $("#wakeTime").val();
-    console.log($bedTime, $wakeTime);
-    console.log(typeof $bedTime);
+    //console.log($bedTime, $wakeTime);
+    //console.log(typeof $bedTime);
     const bedTime = lifeApp.convertTime($bedTime);
     const wakeTime = lifeApp.convertTime($wakeTime);
-    console.log(lifeApp.calculateTime(bedTime, wakeTime));
+    //console.log(lifeApp.timeSlept(bedTime, wakeTime));
     $wrapper.html(lifeApp.sleepOneHtml);
-    lifeApp.updateSleep();
+    localStorage.setItem("day" + dayOfWeek, lifeApp.timeSlept(bedTime, wakeTime));
+    lifeApp.updateGraph();
 });
-
 
 
 // function to convert inputted time (string) into hours (num)
@@ -79,7 +84,7 @@ lifeApp.convertTime = (inputtedTime) => {
 };
 
 // function to calculate total time slept
-lifeApp.calculateTime = (sleep, wake) => {
+lifeApp.timeSlept = (sleep, wake) => {
     const diff = wake - sleep;
     if (diff < 0) {
         return (diff + 24).toFixed(2);
@@ -88,25 +93,85 @@ lifeApp.calculateTime = (sleep, wake) => {
     };
 };
 
-// function to push time slept into sleep tracker array
+// function to push time slept into sleep array
 
 
 // function to update graph
-lifeApp.updateSleep = () => {
+lifeApp.updateGraph = () => {
     $(".sleepOne li").each(function (index, li) {
-        $(li).height(lifeApp.sleepTracker[index] * 25);
+        $(li).height(localStorage.getItem("day" + index) * 25);
     })
 };
 
 
-
 // function to calculate average time slept
+lifeApp.averageSlept = () => {
+
+}
+
+
+// -------------------- GOALS -----------------------//
+
+const goalsCompleted = 10;
+const $goalsForm = $(".goalsOne form");
+
+lifeApp.goalsOneHtml = `
+    <section class="goalsOne">
+        <h2>goals breakdown</h2>
+        <h3>${goalsCompleted}</h3>
+        <form>
+            <label for="goal">Enter a goal</label>
+            <input type="text" id="goal" placeholder="Eat more cake">
+            <button type="submit">Add a new goal</button>
+        </form>
+        <div id="goalsError">Cannot enter a blank goal</div>
+        <ul class="list"></ul>
+    </section>
+`;
+
+$goalsButton.on("click", function () {
+    $wrapper.html(lifeApp.goalsOneHtml);
+});
+
+
+
+$wrapper.on("submit", ".goalsOne form", function(e){
+    e.preventDefault();
+    let goal = $('.goalsOne input').val();
+    console.log(goal);
+
+    // to delete goal
+    const deleteGoalButton = `<button class="goalDeleteButton"><i class="fas fa-trash-alt"></i></button>`;
+
+    if (goal.replace(/\s/g, '').length) {
+        $('#goalsError').hide();
+        $('.goalsOne ul').append(`<li>${goal}${deleteGoalButton}</li>`);
+        $('.goalsOne input').val('');
+    }else{
+        $('#goalsError').show();
+    };
+});
+
+
+$wrapper.on("click", ".goalsOne li", function () {
+    $(this).toggleClass("strikethrough");
+});
+
+$wrapper.on("click", ".goalDeleteButton", function(){
+    const goalListItem = $(this).parent();
+    goalListItem.remove();
+});
 
 
 
 
 lifeApp.init = () => {
-
+    //localStorage.clear();
+    for (i = 0; i < 7; i++) {
+        if (localStorage.getItem("day" + i) == null) {
+            localStorage.setItem("day" + i, 0);
+        }
+    }
 }
 
 $(function () {
